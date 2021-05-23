a2bff998cc8623bedb63a350acda6e76d32f4a47
Lars <Lars.Git.Johansson@outlook.com>
2021-05-23 14:37:11 +0200

    Add Lars was here

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -85,6 +85,7 @@ class HighwayEnv(AbstractEnv):
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
             else self.vehicle.lane_index[2]
         scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
+        # Lars was here
         reward = \
             + self.config["collision_reward"] * self.vehicle.crashed \
             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
